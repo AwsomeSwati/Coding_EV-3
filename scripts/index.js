77c8732649d5ea9c0e7c0953d7a361fd02b1e417
AwsomeSwati <swati1993kushwaha@gmail.com>
2022-04-27 22:06:40 +0530

    errors rectififyied

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,25 @@
 // Store the wallet amount to your local storage with key "amount"
 
+
+let wallet_amount = JSON.parse(localStorage.getItem("amount")) || []
+
+let wallet_total =  document.getElementById("wallet")
+wallet_total .innerText = wallet_amount
+
 function addToWallet(){
 
-   let wallet_amount = JSON.parse(localStorage.getItem("amount")) || []
+ //  let wallet_amount = JSON.parse(localStorage.getItem("amount")) || []
      
+   wallet_total .innerText = wallet_amount
     let input_amt = document.getElementById("amount").value 
 
 
    var total = Number( wallet_amount) + Number(input_amt);
  
    console.log(total)
-
-    let wallet_total =  document.getElementById("wallet")
+       
+    //let wallet_total =  document.getElementById("wallet")
      wallet_total .innerText = total
-
+   
      localStorage.setItem("amount",JSON.stringify(total))
 }
